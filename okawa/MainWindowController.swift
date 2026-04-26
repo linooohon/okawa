d48116623c06d06d87ba66f43ae83098dff8b6fd
linooohon <linooohonjp@gmail.com>
2026-04-26 21:05:08 +0900

    fix: replace deprecated activate(ignoringOtherApps:) and activate(options:)
    Use NSApp.activate() and NSRunningApplication.activate(from:) on macOS 14+,
    falling back to the older APIs on macOS 13.

--- okawa/MainWindowController.swift
+++ okawa/MainWindowController.swift
@@ -9,7 +9,11 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
   func showAndActivate(_ sender: AnyObject?) {
     self.showWindow(sender)
     self.window?.makeKeyAndOrderFront(sender)
-    NSApp.activate(ignoringOtherApps: true)
+    if #available(macOS 14.0, *) {
+      NSApp.activate()
+    } else {
+      NSApp.activate(ignoringOtherApps: true)
+    }
   }
 
   func windowWillClose(_ notification: Notification) {
@@ -19,6 +23,10 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
   func deactivate() {
     // focus an application owning the menu bar
     let workspace = NSWorkspace.shared
-    workspace.menuBarOwningApplication?.activate(options: NSApplication.ActivationOptions.activateIgnoringOtherApps)
+    if #available(macOS 14.0, *) {
+      workspace.menuBarOwningApplication?.activate(from: NSRunningApplication.current)
+    } else {
+      workspace.menuBarOwningApplication?.activate(options: .activateIgnoringOtherApps)
+    }
   }
 }
